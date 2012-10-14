de71afb7ab33b762d060d4486a89326bfad9d595
PlayfulGod <playfulgod@gmail.com>
2012-10-14 19:49:15 -0400

    fix cm.mk

--- cm.mk
+++ cm.mk
@@ -1,13 +1,15 @@
 ## Specify phone tech before including full_phone
 $(call inherit-product, vendor/cm/config/cdma.mk)
 
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 $(call inherit-product, device/common/gps/gps_us_supl.mk)
 
--include vendor/cm/config/common_versions.mk
+-include vendor/cm/config/common.mk
 
 # Inherit device configuration
 $(call inherit-product, device/zte/arthur/device_arthur.mk)
@@ -17,9 +19,12 @@ PRODUCT_BRAND := BoostMobile
 PRODUCT_DEVICE := arthur
 PRODUCT_MODEL := N860
 PRODUCT_MANUFACTURER := ZTE
+PRODUCT_CHARACTERISTICS := phone
 
 # Release name and versioning
 PRODUCT_RELEASE_NAME := arthur
+
+TARGET_BOOTANIMATION_NAME := vertical-320x480
 
 
 
